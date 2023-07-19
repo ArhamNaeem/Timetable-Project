99227f01a2461e08a929284c3312ae1432cf4db4
Arham Naeem <arhamn205@gmail.com>
2023-07-19 13:04:50 +0500

    console work done

--- timetable_generation.py
+++ timetable_generation.py
@@ -1,8 +1,6 @@
 
 import random
 from tabulate import tabulate
-
-
 class TimetableGenerator:
     def __init__(self, subjects, teachers, max_lectures_per_day,teacherSubjectsTaught , rooms, days, time_slots):
         self.subjects = subjects
@@ -28,17 +26,14 @@ class TimetableGenerator:
         _semester = 1
         for subject_list in self.subjects:
             for subject in subject_list:
-            #  print(subject)
             #get relevant teachers teaching the course
              available_teachers = [teacher for teacher,subjects_teaching in teacherSubjectsTaught.items() if subject in subjects_teaching]
-            #  print(f'teachers available for {subject} are {available_teachers}')
              self.selected_teachers.append({"teacher": available_teachers[random.randint(0,len(available_teachers)-1)],"subject":subject,"semester":_semester })
             _semester+=1
         for selected_teacher in self.selected_teachers:
          teacher = selected_teacher['teacher']
          subject = selected_teacher['subject']
          semester = selected_teacher['semester']
-         print(teacher,subject,"Sem",semester)
          for day in self.days:
             subject_taught = []
             tries = 10000
@@ -69,18 +64,50 @@ class TimetableGenerator:
                             "time_slot":random_time_slot
                         })
                        
-                tries-=1
-        print('Class timetable')
-        for _ in ct:
-            print(_)
-        print('--------------------------------')
-        print('Teacher timetable')
+                tries-=1 
+        return ct,tt
+    def print_semesterwise_timetable(self,timetable):
+        print("--------Semester-Wise Timetable-------")
+        timetable.sort(key=lambda x: x['semester'])
         
-        for _ in tt:
-            print(_)
-
-
-        
+        semester_data = {}
+        previous_semeter = None
+        for row in timetable : 
+            semester = row['semester']
+            if semester!= previous_semeter:
+                previous_semeter = semester
+                semester_data[semester] = []
+            semester_data[semester].append(row)
+        headers = [key for key in timetable[0].keys()  if key !='semester']  # Assuming all dictionaries have the same keys
+        # # Convert the dictionaries to lists of values to create the table
+        for semester, data in semester_data.items():
+                print(f'Timetable for semester {semester}')
+                        
+                table_data = [[row[key] for key in headers] for row in data]
+            
+        # # Use the tabulate function to format and print the table
+                print(tabulate(table_data, headers=headers, tablefmt="grid"))
+    
+    def print_teacher_timetable(self,timetable):
+        print("-------Teacher's Timetable-----------")
+        previous_teacher = None
+        teacher_data = {}
+        for row in timetable:
+            teacher = row['teacherName']
+            subject = row['subject']
+            day = row['day']
+            semester = row['semester']
+            if previous_teacher != teacher:
+                teacher_data[teacher] = []
+                previous_teacher = teacher
+            teacher_data[teacher].append(row)
+        # print(teacher_data)
+        headers = [key for key in timetable[0].keys() if key!='teacherName']
+        for teacher, data in teacher_data.items():
+            print(f'Timetable for teacher {teacher}')
+            table_data = [[row[key] for key in headers] for row in data]
+            print(tabulate(table_data, headers=headers, tablefmt="grid"))
+            
     def _initialize_timetables(self):
     #every room is initially empty 
         for room in self.rooms:
@@ -136,7 +163,6 @@ semester_subjects = [["OOP","Programming Fundamentals","English","ICT","History"
 
 
 teachers = ["Mr. Smith", "Ms. Johnson", "Mr. Brown", "Ms. Davis", "Mr. Wilson", "Mrs. Anderson", "Mr. Thompson", "Ms. Roberts", "Mr. Clark", "Mrs. Moore"]
-max_lectures_per_day = 4
 
 teacherSubjectsTaught = {
     'Mr. Smith' : ['COAL','OOP'],
@@ -155,5 +181,6 @@ rooms = ["Room 1", "Room 2", "Room 3", "Room 4", "Room 5", "Room 6", "Room 7", "
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 time_slots = ["9:00 AM - 10:00 AM", "10:00 AM - 11:00 AM", "11:00 AM - 12:00 PM", "1:00 PM - 2:00 PM", "2:00 PM - 3:00 PM"]
 timetable_generator = TimetableGenerator(semester_subjects, teachers, max_lectures_per_day, teacherSubjectsTaught, rooms, days, time_slots)
-timetable_generator.generate_timetable()
-
+class_timetable, teacher_timetable = timetable_generator.generate_timetable()
+timetable_generator.print_semesterwise_timetable(class_timetable)
+timetable_generator.print_teacher_timetable(teacher_timetable)
